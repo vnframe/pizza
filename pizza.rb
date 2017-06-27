7cd1ed28691a4472115bef8abd0973c5d3895682
vnframe <vnframe95@icloud.com>
2017-06-27 14:04:20 -0400

    pizza partial

---
+++ pizza.rb
@@ -0,0 +1,47 @@
+def veggies
+	veggies = ["peppers", "onions", "spinach"].sample 
+end
+
+def crust_type
+  crust_type = ["plain crust", "cheesy crust", "italian herbs crust"].sample
+end
+
+def cheese
+   cheese = ["mozzarella", "cheddar", "blend"].sample
+end
+
+def sauce
+	sauce = ["original marinara", "spicy marinara", "alfredo", "BBQ"].sample
+end
+
+def meat
+	meat = ["pepperoni", "sausage", "bacon", "ham", "chicken"].sample
+end
+
+def size
+	size = ["med", "large", "extra large", "super mega large"].sample
+end
+
+
+def how_many?
+	print "how many pizzas would you like?"
+	x = gets.chomp.to_i
+end
+pizza = 0
+x = how_many?
+
+x.times do 
+
+pizza += 1
+puts "PIZZA " + pizza.to_s
+puts size
+puts crust_type
+puts sauce
+puts cheese
+puts veggies
+puts meat
+end
+
+puts ""
+y = 9.99
+print "Total: $" , (x * y.to_f), "\n"
